9f69eac6708737a389255341ab30bff221fefdc3
Carlos Aguirre <carlosaguirre@Carloss-MacBook-Air.local>
2024-11-04 17:43:03 -0600

    saving work

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html') #Pagina principal
+    return render_template('index.html')
 
 @app.route('/quienes_somos')
 def quienes_somos():
-    return render_template('quienes_somos.html') #Pagina de quienes somos
+    return render_template('quienes_somos.html')
 
-@app.route('/servicios')
-def servicios():
-    return render_template('servicios.html') #Pagina de Servicios
+@app.route('/nuestros_servicios')
+def nuestros_servicios():
+    return render_template('nuestros_servicios.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
